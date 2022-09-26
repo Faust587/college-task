d983b401f6ee53865013b1857b04c8ac199a4a09
Yura Synkulych <kri3g0vec@gmail.com>
2022-09-26 09:24:54 +0300

    update data.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import text from "./assets/data/data.json";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import text from "./assets/data/data1.json";
 
 function App() {
 
@@ -11,14 +12,21 @@ function App() {
   }
 
   return (
-    <div className="app-container">
-      <div className="text">
-        {text[counter]}
-      </div>
-      <div className="button" onClick={click}>
-        <a />
-      </div>
-    </div>
+
+  <BrowserRouter basename="/college-task/">
+    <Routes>
+      <Route path="/" element={
+        <div className="app-container">
+          <div className="text">
+            {text[counter]}
+          </div>
+          <div className="button" onClick={click}>
+            <a />
+          </div>
+        </div>
+      } />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
